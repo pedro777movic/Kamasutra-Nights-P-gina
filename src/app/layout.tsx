77de7f97980a8e57a40c756e5b79ebed9fa9f203
Pedro Victor Barbosa <pedromovic2017@gmail.com>
2025-10-26 23:48:27 +0000

    Adicione corretamente esse código pixel abaixo na página de vendas para

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
+import Script from 'next/script';
 
 
 export const metadata: Metadata = {
@@ -24,6 +25,16 @@ export default function RootLayout({
       <body className="font-body antialiased">
         {children}
         <Toaster />
+        <Script id="utmify-pixel" strategy="afterInteractive">
+          {`
+            window.pixelId = "68fe29ffeace3b383911050e";
+            var a = document.createElement("script");
+            a.setAttribute("async", "");
+            a.setAttribute("defer", "");
+            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+            document.head.appendChild(a);
+          `}
+        </Script>
       </body>
     </html>
   );
