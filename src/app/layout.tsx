3a0c9567e5bafe54451c6564df14549eb2fd005f
Pedro Victor Barbosa <pedromovic2017@gmail.com>
2025-11-09 14:50:33 +0000

    Adicione mais um pixel nessa página de vendas, o  Pixel do  Meta, adicio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,8 +30,20 @@ export default function RootLayout({
         ></Script>
       </head>
       <body className="font-body antialiased">
+        {/* Meta Pixel Fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1478272150070620&ev=PageView&noscript=1"
+          />
+        </noscript>
+        
         {children}
         <Toaster />
+
+        {/* Utmify Pixel */}
         <Script id="utmify-pixel" strategy="afterInteractive">
           {`
             window.pixelId = "690f7f51110a6c57fb339922";
@@ -40,6 +52,22 @@ export default function RootLayout({
             a.setAttribute("defer", "");
             a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
             document.head.appendChild(a);
+          `}
+        </Script>
+
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1478272150070620');
+            fbq('track', 'PageView');
           `}
         </Script>
       </body>
